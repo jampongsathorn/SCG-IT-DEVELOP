639a77d14e0b63dfde81f331aee4c7e6e1cc67f1
Pongsathorn <85749998+jampongsathorn@users.noreply.github.com>
2023-09-25 02:05:36 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-
-// JavaScript function to increment the cart counter
-// JavaScript function to increment the cart counter
 function addToCart() {
     // Get the cart counter element by its id
     var cartCounter = document.getElementById("cart-counter");
